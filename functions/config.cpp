67ebf0f011f40c721014d3c26ae71cd8750d174f
Андрей Макаров <mr.wertmax@gmail.com>
2017-10-15 22:04:30 +0400

    add settings for windows encoding to config

--- functions/config.cpp
+++ functions/config.cpp
@@ -1,8 +1,11 @@
 #include <libintl.h>
+#include <windows.h>
 #include "functions.h"
 
 void config () {
     setlocale(LC_ALL, "Russian");
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
     setColor();
     cleanConsole();
 }
